bda473ff192cb1c71c0bd71e7868aa5bf42d2079
tigelek <ordersfeed113579@gmail.com>
2024-04-19 08:53:27 +0500

    lox

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    lox
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,20 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
 
-
+  app: {
+    head: {
+      link: [{
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' }
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+          tagPosition:"bodyClose"
+        }
+      ],
+    },
+}
 
 
 
